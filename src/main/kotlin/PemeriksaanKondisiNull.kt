2b655da8826129784335c9c9a7c0cc4896f82aae
suciifrndzz <sucizulianafernandes298@gmail.com>
2024-09-10 20:35:47 +0700

    Add PemeriksaanKondisiNull.kt Add Safecalls.kt

---
+++ src/main/kotlin/PemeriksaanKondisiNull.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    //conditions
+    var maybeNumber: Int? = 15
+    if (maybeNumber != null)
+        println(maybeNumber * 2)
+    else
+        println("The entered value isn't a number")
+}
---
+++ src/main/kotlin/Safecalls.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    //safe calling
+    //?/Let
+    var maybeNumber: Int? = 15
+    maybeNumber?.let { println(it) }
+
+    //the ?.chaining
+    //student?.teacher?/supervisor?.name = "seymour Skinner"
+}
