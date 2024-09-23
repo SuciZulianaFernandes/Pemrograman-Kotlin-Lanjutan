f92d71d5bbcb1ebd0f8fe16375f0d8dd3b27157f
suciifrndzz <sucizulianafernandes298@gmail.com>
2024-09-23 17:15:47 +0700

    Add ArrayStandarKotlin.kt Add Operator!!.kt Add Operator!!2.kt

---
+++ src/main/kotlin/Operator!!2.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    //Null safety
+    //the !! operator
+    var maybeNumber: Int? = 25
+   // println(maybeNumber! * 2)
+
+    maybeNumber = null
+  //  println(maybeNumber!! * 2)
+}
